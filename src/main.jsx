7ad4cf914773dcfa3038078e4047a984f829f67c
Juan Pandales <89496222+JuanDPandales@users.noreply.github.com>
2023-02-21 17:43:39 -0500

    Cambio de estructura del main.jsx, mejor lectura

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App  from '../src/App.jsx'
+import { createRoot } from "react-dom/client";
+import App from "../src/App.jsx";
 import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-
+root.render(
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
