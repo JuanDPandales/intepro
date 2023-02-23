8a0d0de57bed066b01696c47c1fb94d409c24d21
Juan Pandales <89496222+JuanDPandales@users.noreply.github.com>
2023-02-23 16:10:53 -0500

    Footer siempre al final de la pantalla + About

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+
+
+export default function About() {
+  return (
+    <>
+    <div className="flex justify-center items-center pt-24 font-semibold">
+
+    </div>
+    <Footer/>
+    </>
+  )
+}
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,16 +3,16 @@ import React from "react";
 export default function Footer() {
   return (
     <>
-      <footer className="relative -bottom-64 overflow-y-hidden py-6 bg-gray-200 text-gray-900">
+      <footer className="absolute bottom-0 w-full overflow-y-hidden py-6 bg-gray-200 text-gray-900">
         <div className="container px-6 mx-auto space-y-6 divide-y divide-gray-400 md:space-y-12 divide-opacity-50">
           <div className="grid justify-center lg:justify-center">
             <div className="flex flex-col self-center text-sm text-center md:block lg:col-start-1 md:space-x-6">
-              <span>El papá de los footers</span>
+              <span>*Redireccionar a mi Github*</span>
               <a rel="noopener noreferrer" href="#">
-                <span>El mejor footer que he hecho</span>
+                <span>*Redireccionar al proyecto</span>
               </a>
               <a rel="noopener noreferrer" href="#">
-                <span>Chao</span>
+                <span>*Contacto*</span>
               </a>
             </div>
           </div>
